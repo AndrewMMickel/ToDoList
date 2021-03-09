a2663eab1c98f33f399ae4787b2adb2acb93a041
AndrewMMickel <andrew.m.mickel@gmail.com>
2021-03-09 15:06:02 -0800

    update code for database relationship

--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -4,8 +4,14 @@ namespace ToDoList.Models
 {
     public class ToDoListContext : DbContext
     {
-        public DbSet<Item> Items { get; set; }
+        public virtual DbSet<Category> Categories { get; set; } //new line
+        public virtual DbSet<Item> Items { get; set; }          //changed
 
         public ToDoListContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+        }
     }
 }
